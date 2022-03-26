6b19327947cf26c6716f06c5567aba40b6a0016a
Gabriel <GABEHJ2001@GMAIL.COM>
2022-03-26 12:47:06 -0700

    Tkinter v.1
    Not quite done with this, but I need to grab text from the textbox with the 'Generate' button. I also need to write out the html for the project

---
+++ main.py
@@ -0,0 +1,40 @@
+
+import webbrowser
+from tkinter import *
+import tkinter as tk
+
+
+class ParentWindow(Frame):
+    def __init__(self, master, *args, **kwargs):
+        Frame.__init__(self, master, *args, **kwargs)
+    #set master
+        self.master = master
+        self.master.geometry('{}x{}'.format(500,500))
+
+    #buttons
+        #generate
+        self.btn1=Button(self.master, text='Generate', width=10, height=1, command=self.callback)
+        self.btn1.grid(row=0, column=0, padx=5, pady=5)
+        #close
+        self.btn2=Button(self.master, text='Close', width=10, height=1, command=self.close)
+        self.btn2.grid(row=1, column=0, padx=5, pady=5)
+
+    #textbox
+        self.vartxt1 = StringVar()
+        self.txt1=Entry(self.master, text=self.vartxt1, width=50, bg='lightgray')
+        self.txt1.grid(row=0, column=1, columnspan=3, padx=5, pady=10)
+
+
+
+    #definitions
+    def close(self):
+        self.master.destroy()   #closes program
+
+    def callback():
+        print('self.vartxt1.get()', self.vartxt1.get())
+
+
+if __name__ == '__main__':
+    root=tk.Tk()
+    App=ParentWindow(root)
+    root.mainloop()
